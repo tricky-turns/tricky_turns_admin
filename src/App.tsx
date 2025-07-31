2fcd3351caa767661fe1c11d3885b8116e6baeab
Ahmed Hamed <ahamed@li-4c4c4544-0033-4210-8031-b5c04f525733.ibm.com>
2025-07-30 21:33:38 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // src/App.tsx
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Modes from "./pages/Modes";
